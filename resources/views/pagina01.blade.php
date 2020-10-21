d7d17b0d65ab2da48356ba04fd718454a71bebe7
fab24x <fabio12_000@hotmail.com>
2020-10-21 20:00:57 +0200

    Version final

--- resources/views/pagina01.blade.php
+++ resources/views/pagina01.blade.php
@@ -17,6 +17,6 @@
     <a href="{{ url('pagina02/Historia') }}" class="=btn btn-xs btn-info pull-rigth">HISTORIA</a>
     <a href="{{ url('pagina02/Economia') }}" class="=btn btn-xs btn-info pull-rigth">ECONOMIA</a>
     <a href="{{ url('pagina02/Ingles') }}" class="=btn btn-xs btn-info pull-rigth">INGLES</a>
-
+    <a href="{{ url('api/preguntas', ['Historia']) }}" class="btn btn-xs btn-info pull-rigth">Historia pero un json solo </a>
 
 @endsection
